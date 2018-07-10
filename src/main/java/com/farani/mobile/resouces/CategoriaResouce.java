f60c6c77e6e14106a3c2a8552e0a88b10dba0239
webfaria <web_faria@yahoo.com.br>
2018-07-10 15:26:22 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/farani/mobile/resouces/CategoriaResouce.java
+++ src/main/java/com/farani/mobile/resouces/CategoriaResouce.java
@@ -1,15 +1,28 @@
 package com.farani.mobile.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.farani.mobile.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
-		return "REST está funcionando";
+	public List<Categoria> lista() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
