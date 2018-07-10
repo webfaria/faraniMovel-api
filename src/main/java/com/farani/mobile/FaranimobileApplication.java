754a6cbbad5bf263a265a437a31ee32ae7df4381
webfaria <web_faria@yahoo.com.br>
2018-07-09 23:46:03 -0300

    Projeto criado

---
+++ src/main/java/com/farani/mobile/FaranimobileApplication.java
@@ -0,0 +1,12 @@
+package com.farani.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaranimobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaranimobileApplication.class, args);
+	}
+}
