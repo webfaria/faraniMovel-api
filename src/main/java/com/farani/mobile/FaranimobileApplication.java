3933afa536cbadf48d51f79897f5cea51ba6e989
webfaria <web_faria@yahoo.com.br>
2018-07-12 18:33:17 -0300

    Estado e Cidade

--- src/main/java/com/farani/mobile/FaranimobileApplication.java
+++ src/main/java/com/farani/mobile/FaranimobileApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.farani.mobile.domain.Categoria;
+import com.farani.mobile.domain.Cidade;
+import com.farani.mobile.domain.Estado;
 import com.farani.mobile.domain.Produto;
 import com.farani.mobile.repositories.CategoriaRepository;
+import com.farani.mobile.repositories.CidadeRepository;
+import com.farani.mobile.repositories.EstadoRepository;
 import com.farani.mobile.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,6 +21,12 @@ public class FaranimobileApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -34,16 +44,29 @@ public class FaranimobileApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computado", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
