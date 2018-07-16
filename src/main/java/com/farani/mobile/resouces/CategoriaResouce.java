1a5ce84271508148379c101eb19e40d1a54b318b
webfaria <web_faria@yahoo.com.br>
2018-07-16 08:51:09 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/farani/mobile/resouces/CategoriaResouce.java
+++ src/main/java/com/farani/mobile/resouces/CategoriaResouce.java
@@ -1,11 +1,15 @@
 package com.farani.mobile.resouces;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.farani.mobile.domain.Categoria;
 import com.farani.mobile.services.CategoriaService;
@@ -21,5 +25,14 @@ public class CategoriaResouce {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria categoria = categoriaService.buscar(id);
 		return ResponseEntity.ok(categoria);
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> Insert(@RequestBody Categoria obj) {
+		obj = categoriaService.insert(obj);
+		// Recupera Uri no momento da inserção
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/farani/mobile/services/CategoriaService.java
+++ src/main/java/com/farani/mobile/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return categoria.orElseThrow(() -> new ObjectNotFoundExeception(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
 }
