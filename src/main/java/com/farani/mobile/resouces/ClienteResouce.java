c4ba4a843f8a1f6398d4f4130f2fc8023eebaaff
webfaria <web_faria@yahoo.com.br>
2018-07-17 13:36:03 -0300

    PUT, DELETE, GET para Cliente

--- src/main/java/com/farani/mobile/resouces/ClienteResouce.java
+++ src/main/java/com/farani/mobile/resouces/ClienteResouce.java
@@ -1,13 +1,24 @@
 package com.farani.mobile.resouces;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.farani.mobile.domain.Cliente;
+import com.farani.mobile.dto.ClienteDTO;
 import com.farani.mobile.services.ClienteService;
 
 @RestController
@@ -21,6 +32,52 @@ public class ClienteResouce {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente cliente = clienteService.find(id);
 		return ResponseEntity.ok(cliente);
+	}
+	
+	//CADASTRO
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
+		Cliente obj = clienteService.fromDTO(objDto);
+		obj = clienteService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	//ATUALIZAÇÃO
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = clienteService.fromDTO(objDto);
+		obj.setId(id);
+		obj = clienteService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//EXCLUSÃO
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//PESQUISA TODOS
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = clienteService.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	//PAGINAÇÃO
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	
--- src/main/java/com/farani/mobile/services/ClienteService.java
+++ src/main/java/com/farani/mobile/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.farani.mobile.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.farani.mobile.domain.Cliente;
+import com.farani.mobile.dto.ClienteDTO;
 import com.farani.mobile.repositories.ClienteRepository;
+import com.farani.mobile.services.execeptions.DataIntegrityException;
 import com.farani.mobile.services.execeptions.ObjectNotFoundExeception;
 
 @Service
@@ -20,4 +27,43 @@ public class ClienteService {
 		return cliente.orElseThrow(() -> new ObjectNotFoundExeception(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return clienteRepository.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque ha entidade relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
+
