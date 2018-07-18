72d48c108633f550e65f18380d0bc99bb68250d7
webfaria <web_faria@yahoo.com.br>
2018-07-18 13:11:48 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/farani/mobile/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/farani/mobile/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.farani.mobile.domain.Cliente;
 import com.farani.mobile.domain.enums.TipoCliente;
 import com.farani.mobile.dto.ClienteNewDTO;
+import com.farani.mobile.repositories.ClienteRepository;
 import com.farani.mobile.resouces.exeption.FieldMessage;
 import com.farani.mobile.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
