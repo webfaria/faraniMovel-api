e60afa8c6969788f4dda2520d2007b14b216a004
webfaria <web_faria@yahoo.com.br>
2018-07-18 13:42:33 -0300

    Validação customizada: email não repetido na atualização do cliente

---
+++ src/main/java/com/farani/mobile/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.farani.mobile.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.farani.mobile.domain.Cliente;
+import com.farani.mobile.dto.ClienteDTO;
+import com.farani.mobile.repositories.ClienteRepository;
+import com.farani.mobile.resouces.exeption.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request
+				.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null && !cliente.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
