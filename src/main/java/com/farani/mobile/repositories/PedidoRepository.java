f60cb975b0438e0f509a5cf73239aa2911954fc1
webfaria <web_faria@yahoo.com.br>
2018-07-25 19:30:22 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/farani/mobile/repositories/PedidoRepository.java
+++ src/main/java/com/farani/mobile/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.farani.mobile.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.farani.mobile.domain.Cliente;
 import com.farani.mobile.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/farani/mobile/services/PedidoService.java
+++ src/main/java/com/farani/mobile/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.farani.mobile.domain.Cliente;
 import com.farani.mobile.domain.ItemPedido;
 import com.farani.mobile.domain.PagamentoComBoleto;
 import com.farani.mobile.domain.Pedido;
@@ -14,6 +18,8 @@ import com.farani.mobile.domain.enums.TipoPagamento;
 import com.farani.mobile.repositories.ItemPedidoRepository;
 import com.farani.mobile.repositories.PagamentoRepository;
 import com.farani.mobile.repositories.PedidoRepository;
+import com.farani.mobile.security.UserSS;
+import com.farani.mobile.services.exceptions.AuthorizationException;
 import com.farani.mobile.services.exceptions.ObjectNotFoundException;
 
 
@@ -71,4 +77,13 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
 }
