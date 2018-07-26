be379cd931c606a9152a63602a00b24e92c8a58d
webfaria <web_faria@yahoo.com.br>
2018-07-26 13:48:50 -0300

    Refresh token

---
+++ src/main/java/com/farani/mobile/resouces/AuthResource.java
@@ -0,0 +1,31 @@
+package com.farani.mobile.resouces;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farani.mobile.security.JWTUtil;
+import com.farani.mobile.security.UserSS;
+import com.farani.mobile.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+
+}
