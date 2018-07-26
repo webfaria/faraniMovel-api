3c4ddb9a2570a6cac68f3d768cae591fbdd4a263
webfaria <web_faria@yahoo.com.br>
2018-07-26 17:38:06 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/farani/mobile/FaranimobileApplication.java
+++ src/main/java/com/farani/mobile/FaranimobileApplication.java
@@ -1,11 +1,17 @@
 package com.farani.mobile;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.farani.mobile.services.S3Service;
+
 @SpringBootApplication
 public class FaranimobileApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FaranimobileApplication.class, args);
@@ -13,7 +19,7 @@ public class FaranimobileApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("D:\\drole\\imagens\\IMG-20160607-WA0020.jpg");
 	}
 
 }
