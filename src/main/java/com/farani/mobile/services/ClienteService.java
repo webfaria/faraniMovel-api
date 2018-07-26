40161e2b4c8b7176077fcf9c070a9563862c441b
webfaria <web_faria@yahoo.com.br>
2018-07-26 19:37:22 -0300

    Enviando imagem via endpoint

--- src/main/java/com/farani/mobile/services/ClienteService.java
+++ src/main/java/com/farani/mobile/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.farani.mobile.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.farani.mobile.domain.Cidade;
 import com.farani.mobile.domain.Cliente;
@@ -32,25 +34,28 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
+	@Autowired
+	private S3Service s3Service;
+
 	public Cliente find(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
-		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
@@ -73,38 +78,44 @@ public class ClienteService {
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
-	
+
 	public List<Cliente> findAll() {
 		return clienteRepository.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return clienteRepository.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cidade = new Cidade(objDto.getCidadeId(), null, null);
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cliente, cidade);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cliente, cidade);
 		cliente.getEnderecos().add(end);
 		cliente.getTelefones().add(objDto.getTelefone1());
-		if (objDto.getTelefone2()!=null) {
+		if (objDto.getTelefone2() != null) {
 			cliente.getTelefones().add(objDto.getTelefone2());
 		}
-		if (objDto.getTelefone3()!=null) {
+		if (objDto.getTelefone3() != null) {
 			cliente.getTelefones().add(objDto.getTelefone3());
 		}
 		return cliente;
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-}
 
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+
+		return s3Service.uploadFile(multipartFile);
+	}
+}
--- src/main/java/com/farani/mobile/services/S3Service.java
+++ src/main/java/com/farani/mobile/services/S3Service.java
@@ -1,17 +1,20 @@
 package com.farani.mobile.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.farani.mobile.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -24,7 +27,7 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-/*	public URI uploadFile(MultipartFile multipartFile) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
 			String fileName = multipartFile.getOriginalFilename();
 			InputStream is = multipartFile.getInputStream();
@@ -45,21 +48,6 @@ public class S3Service {
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
 			throw new FileException("Erro ao converter URL para URI");
-		}
-	}*/
-	public void uploadFile(String localFilePath) {
-		try {
-		File file = new File(localFilePath);
-		LOG.info("Iniciando upload");
-		s3client.putObject(new PutObjectRequest(bucketName, "teste.jpg",file ));
-		LOG.info("Finalizado upload");
-		} 
-		catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status code: " + e.getMessage());
-		}
-		catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: " + e.getMessage());
 		}
 	}
 }
